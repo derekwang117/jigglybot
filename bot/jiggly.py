dc942e6eb65222bdb886527ce0ca21dc59950b3f
Derek Wang <toderekwang@gmail.com>
2021-05-20 16:19:38 -0400

    i made mistake
    fixed which token lmao

--- bot/jiggly.py
+++ bot/jiggly.py
@@ -7,7 +7,7 @@ from discord.ext import commands
 
 bot = commands.Bot(command_prefix=".")
 bot.remove_command('help')
-token = os.environ.get('discord_bot_dev_token')
+token = os.environ.get('discord_bot_jiggly_token')
 
 
 @bot.event
